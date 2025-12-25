eddf872377950650b94f44cbb2b1cefe115b41c7
mariana <maranamelen13@gmail.com>
2025-12-25 18:51:48 +0200

    6-task

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,6 +2,11 @@ function getFirstElement<T>(arr: T[]): T {
     return arr[0];
 }
 
-getFirstElement([1, 2, 3]);           // 1
-getFirstElement(["a", "b", "c"]);     // "a"
-getFirstElement([true, false, true]); // true
+// Виклики функції з явною типізацією дженерика
+const firstNumber = getFirstElement<number>([1, 2, 3]);           // number
+const firstString = getFirstElement<string>(["a", "b", "c"]);     // string
+const firstBoolean = getFirstElement<boolean>([true, false, true]); // boolean
+
+console.log(firstNumber);  // 1
+console.log(firstString);  // "a"
+console.log(firstBoolean); // true
